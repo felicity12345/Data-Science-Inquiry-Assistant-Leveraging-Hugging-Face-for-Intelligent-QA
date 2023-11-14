8e7bd42fb286b6d2d12a9b483f8c55ee5666238b
Felix Ehis Osawaru <98605136+felicity12345@users.noreply.github.com>
2023-11-14 13:51:17 -0800

    Add files via upload

---
+++ huggingfacellm.py
@@ -0,0 +1,28 @@
+import os
+from langchain.chains import RetrievalQA
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import Chroma
+#from langchain import HuggingFacePipeline
+from langchain.llms.huggingface_pipeline import HuggingFacePipeline
+
+#Document Loader and Splitter 
+loader = UnstructuredFileLoader("./data.txt")
+documents = loader.load()
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+texts = text_splitter.split_documents(documents)
+
+llm = HuggingFacePipeline.from_model_id(model_id="gpt2", task="text-generation", pipeline_kwargs={"temperature":.9, "max_length":1000})
+# Embeddings and Vector_stores
+directory='db'
+embeddings = HuggingFaceEmbeddings()
+docsearch = Chroma(persist_directory=directory, embedding_function=embeddings)
+
+#Chain
+qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=docsearch.as_retriever())
+
+# Prompt
+query = "What is the cost of your data science course?"
+response = qa.run(query)
+print(response)
